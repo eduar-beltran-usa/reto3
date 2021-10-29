fe4184921d86fb9a7c6bd4ef92c0b93a0a4d2451
eduar-beltran-usa <eduar.beltran.mt@correo.usa.edu.co>
2021-10-28 21:23:36 -0500

    Se adiciona solución de reto5

--- src/main/java/com/usa/entities/Motorbike.java
+++ src/main/java/com/usa/entities/Motorbike.java
@@ -24,7 +24,7 @@ public class Motorbike implements Serializable{
     private Integer id;
     private String name;
     private String brand;
-    private String year;
+    private Integer year;
     private String description;
     @ManyToOne
     @JoinColumn(name = "category_id")
